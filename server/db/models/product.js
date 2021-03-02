d530ee5633985b49b3f7b020fa9dc2c01b9166ab
Viral Patel <vpatel621@gmail.com>
2021-03-02 13:27:41 -0500

    Feature Product Model: validation

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,3 +1,62 @@
-function hi() {
-  return null
-}
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue:
+      'https://p0.pikist.com/photos/575/1/blank-vinyl-record-jacket-record-vinyl-album-audio-black-blank-case-cover.jpg'
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  type: {
+    type: Sequelize.ENUM('Record', 'Record Player'),
+    allowNull: false,
+    validate: {
+      requiredFields() {
+        if (this.type === 'Record' && !this.albumTitle) {
+          throw new Error('Requires album title')
+        }
+      }
+    }
+  },
+  // year: {
+  //   type: Sequelize.STRING,
+  //   allowNull: false,
+  //   validate: {
+  //     notEmpty: true,
+  //   },
+  // },
+  albumTitle: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Product
