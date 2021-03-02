2161070d4b6950dffa1e854beba94dc8c4a0b0ab
Yoshie Fujiwara <yoshie.fujiwara@gmail.com>
2021-03-02 16:11:32 -0600

    api/records done

--- server/api/records.js
+++ server/api/records.js
@@ -1,7 +1,50 @@
 const router = require('express').Router()
-const {product} = require('../db/models')
+const {Product} = require('../db/models')
 module.exports = router
 
-router.get('./api/allRecords', async (req, res, next) => {
-  const allRecords = await product.findAll()
+router.get('/', async (req, res, next) => {
+  try {
+    const allRecords = await Product.findAll({where: {type: 'Record'}})
+    res.json(allRecords)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const record = await Product.create(req.body)
+    res.status(201).json(record)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const record = await Product.findByPk(req.params.id)
+    res.json(record)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const record = await Product.find(req.params.id)
+    await record.update(req.body)
+    res.res.status(204).json(record)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const record = await Product.findByPk(req.params.id)
+    await record.destroy()
+    res.json(record)
+  } catch (err) {
+    next(err)
+  }
 })
