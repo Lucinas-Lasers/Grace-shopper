3f9a162efdfb0a13033f6be78728f2e9cb0cbec3
diegoA-66 <abreugdiego@gmail.com>
2021-03-05 22:37:06 -0500

    Changed navbar
    Added new css and animation to buttons.

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,41 +6,55 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div className="navbar">
-    <Link to="/home">
-      <img className="cheeseLogo" src="../logo.jpg" />
-    </Link>
-    <div className="navbarProducts">
-      <div className="products">Products</div>
-      <div className="productsItems">
-        <div className="albums">Albums</div>
-        <div className="recordPlayers">Record Players</div>
+    <div className="navbarContainerLeft">
+      <Link to="/home">
+        <div className="logoContainer">
+          <h3>Laser</h3>
+          <img className="logo" src="../logo.jpg" />
+          <h3>Records</h3>
+        </div>
+      </Link>
+      <div className="navbarProducts">
+        <p>
+          Products
+          <div className="productItems">
+            <Link to="/allRecords">
+              <div className="productOption">Albums</div>
+            </Link>
+            <Link to="/recordplayers">
+              <div className="productOption">Record Players</div>
+            </Link>
+          </div>
+        </p>
       </div>
     </div>
 
-    <nav className="accountManagement">
-      {isLoggedIn ? (
-        <div>
+    {isLoggedIn ? (
+      <div>
+        <div className="navbarLogin">
           {/* The navbar will show these links after you log in */}
-          <a href="#" onClick={handleClick}>
+          <a href="#" onClick={handleClick} className="navbarButton">
             Logout
           </a>
+          <Link to="/cart">
+            <div className="navbarButton">Cart</div>
+          </Link>
         </div>
-      ) : (
-        <div className="loginSignup">
-          {/* The navbar will show these links before you log in */}
-          <div>
-            <Link to="/login">Login</Link>
-          </div>
-          <div>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        </div>
-      )}
-
-      <div>
-        <Link to="/cart">Cart</Link>
       </div>
-    </nav>
+    ) : (
+      <div className="navbarLogin">
+        {/* The navbar will show these links before you log in */}
+        <Link to="/login">
+          <div className="navbarButton">Login</div>
+        </Link>
+        <Link to="/signup">
+          <div className="navbarButton">Sign Up</div>
+        </Link>
+        <Link to="/cart">
+          <div className="navbarButton">Cart</div>
+        </Link>
+      </div>
+    )}
   </div>
 )
 
